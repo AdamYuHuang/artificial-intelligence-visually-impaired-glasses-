4e8ba241a2220cdc5c92fe49a2ad7c18ff422ded
Adam Huang <104631082+AdamYuHuang@users.noreply.github.com>
2022-04-29 21:00:32 +0800

    Add files via upload

---
+++ say.py
@@ -0,0 +1,11 @@
+import pyttsx3
+engine=pyttsx3.init()
+engine.setProperty('voice','zh')
+def say(a):
+    a=str(a)
+    engine.say(a)
+    engine.runAndWait()
+    
+while True:
+	a=input("text")
+	say(a)
